1a53aa0e00742d3d2c47dee1211f49e805147c71
Mario <e1425172@student.tuwien.ac.at>
2020-12-30 21:53:18 +0100

    fix bug where title wouldn't change when using POST requests

---
+++ enhance.js
@@ -0,0 +1,6 @@
+if(document.getElementById('query') !== null) {
+    document.title = document.getElementById('query').value +  ' - Startpage.com';
+}
+else {
+    document.title = document.getElementById('q').value + ' - Startpage.com';
+}
