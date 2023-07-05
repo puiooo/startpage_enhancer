50c32f6c7a5f3e7a6dace2c6ba7de6393c65c781
ueen <ueli.sarnighausen@online.de>
2023-07-05 13:14:43 +0200

    Update enhance.js
    fix menubar

--- enhance.js
+++ enhance.js
@@ -5,12 +5,7 @@ document.title = searchTerm +  " - Startpage.com";
 
 // Append Maps links
 let menubar = document.getElementsByClassName('categories')[0];
-
-let menuItemNr = document.body.textContent.includes('Any size') ? 0 : 1; // dirty workaround ;)
-
-let divItemClass = menubar.children[menuItemNr].classList[0];
-let formItemClass = menubar.children[menuItemNr].children[0].classList[0];
-let buttonItemClass = menubar.children[menuItemNr].children[0].children[menubar.children[0].children[0].children.length-1].classList[0];
+menubar.className = 'categories'; //removes additional styling
 
 // - GMAPS Link
 let googleMapsLink = document.createElement("a");
@@ -18,20 +13,10 @@ googleMapsLink.style = "text-decoration: inherit; color:inherit; white-space: no
 googleMapsLink.text = "Google Maps";
 googleMapsLink.href = "https://maps.google.com?q=" + searchTerm;
 
-let linkDiv = document.createElement("div");
-linkDiv.classList.add(divItemClass);
-
-let linkForm = document.createElement("form");
-linkForm.classList.add(formItemClass);
-linkForm.action = "javascript:void(0);";
-
-let buttonForForm = document.createElement("button");
-buttonForForm.classList.add(buttonItemClass);
-
-buttonForForm.appendChild(googleMapsLink);
-linkForm.appendChild(buttonForForm);
-linkDiv.appendChild(linkForm);
-menubar.appendChild(linkDiv);
+let gmaps = menubar.lastChild.cloneNode(true);
+let gmapsButton = gmaps.firstChild.lastChild;
+gmapsButton.innerHTML = '';
+gmapsButton.appendChild(googleMapsLink);
 
 // - OSM Link
 let osmLink = document.createElement("a");
@@ -39,20 +24,14 @@ osmLink.style = "text-decoration: inherit; color:inherit; white-space: nowrap;";
 osmLink.text = "OpenStreetMap";
 osmLink.href = "https://www.openstreetmap.org/search?query=" + searchTerm;
 
-linkDiv = document.createElement("div");
-linkDiv.classList.add(divItemClass);
+let osmaps = menubar.lastChild.cloneNode(true);
+let osmapsButton = osmaps.firstChild.lastChild;
+osmapsButton.innerHTML = '';
+osmapsButton.appendChild(osmLink);
 
-linkForm = document.createElement("form");
-linkForm.classList.add(formItemClass);
-linkForm.action = "javascript:void(0);";
-
-buttonForForm = document.createElement("button");
-buttonForForm.classList.add(buttonItemClass);
-buttonForForm.appendChild(osmLink);
-
-linkForm.appendChild(buttonForForm);
-linkDiv.appendChild(linkForm);
-menubar.appendChild(linkDiv);
+// - add both
+menubar.appendChild(gmaps);
+menubar.appendChild(osmaps);
 
 
 // Add website-icons
